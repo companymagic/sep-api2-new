c33ab7d7b1941890e437c2773d443fafe4388c59
yasuhiro kashiwaya <president@companymagic.com>
2019-07-27 05:13:01 +0900

    test commit

---
+++ components/footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="footer">
+
+this is footer
+
+    </div>
+</template>
+
+<script>
+export default{
+    name:"bottom"
+}
+
+
+</script>
+
+<style>
+.footer {
+    position:fixed;
+    bottom: 10px;
+    width:100%;
+    background-color: black;
+    color: lightgray;
+}
+
+</style>
